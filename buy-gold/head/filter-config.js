914b54a98cd9a77aa6e4a1a26d1e5c8e19bdf44b
joeright1001 <joe.right1001@gmail.com>
2026-06-18 09:40:42 +1200

    Build and release v1.0.413

--- buy-gold/head/filter-config.js
+++ buy-gold/head/filter-config.js
@@ -150,7 +150,7 @@ window.FILTER_CONFIG = {
     },
     checkbox_investment: {
       attribute: 'data-mint',
-      values: ['Investment Selection']
+      values: ['Value Selection']
     },
     checkbox_nzmint: {
       attribute: 'data-mint',
@@ -190,7 +190,7 @@ window.FILTER_CONFIG = {
     },
     checkbox_investor: {
       attribute: 'data-mint',
-      values: ['Investment Selection']
+      values: ['Value Selection']
     },
     checkbox_collectables: {
       attribute: 'data-product-type',
